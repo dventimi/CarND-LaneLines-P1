c3a4caf241f1c50bdc9e852ad300f12080b60bd9
David A. Ventimiglia <dventimi@gmail.com>
2016-12-02 11:26:04 -0800

    Added more helper functions

--- lanelines.py
+++ lanelines.py
@@ -43,23 +43,53 @@ intercept = lambda lines, m:  lines[:,0,1]-m*lines[:,0,0]
 lidx = lambda slopes:  np.logical_and(np.isfinite(slopes), slopes<0)
 ridx = lambda slopes:  np.logical_and(np.isfinite(slopes), slopes>0)
 
-def process_image(img0, kernel_size=5, low_threshold=50, high_threshold=150, rho=2, theta=1, threshold=50, min_line_length=3, max_line_gap=1):
-    img1 = util.grayscale(img0)
-    img2 = util.gaussian_blur(img1, kernel_size)
-    img3 = util.canny(img2, low_threshold, high_threshold)
-    img4 = util.region_of_interest(img3, road(img0)[:,:,::-1])
-    img5, lines = util.hough_lines(img4, rho, theta*np.pi/180, threshold, min_line_length, max_line_gap)
-    img6 = util.weighted_img(img5, img0)
-    img7 = np.copy(img6)
+def grayscale_image(img, **kwargs):
+    return util.grayscale(img)
+
+def blur_image(img, **kwargs):
+    return util.gaussian_blur(img, kwargs['kernel_size'])
+
+def edge_image(img, **kwargs):
+    return util.canny(img, kwargs['low_threshold'], kwargs['high_threshold'])
+
+def mask_image(img, vertices, **kwargs):
+    return util.region_of_interest(img, vertices)
+
+def houghtransform_image(img, **kwargs):
+    lines = cv2.HoughLinesP(img, kwargs['rho'], kwargs['theta']*np.pi/180, kwargs['threshold'], np.array([]), minLineLength=kwargs['min_line_length'], maxLineGap=kwargs['max_line_gap'])
+    image = util.hough_lines(img, kwargs['rho'], kwargs['theta']*np.pi/180, kwargs['threshold'], kwargs['min_line_length'], kwargs['max_line_gap'])
+    return image, lines
+
+def average_lines(img, lines, **kwargs):
+    image = np.copy(img)
     m = slope(lines)
     b = intercept(lines, m)
     mbar = [np.mean(m[lidx(m)]), np.mean(m[ridx(m)])]
     bbar = [np.mean(b[lidx(m)]), np.mean(b[ridx(m)])]
-    l_pts = roadline_pts(img0, mbar[0], bbar[0])
-    r_pts = roadline_pts(img0, mbar[1], bbar[1])
-    cv2.line(img7, l_pts[0], l_pts[1], (0, 255, 0), 5)
-    cv2.line(img7, r_pts[0], r_pts[1], (0, 255, 0), 5)
+    l_pts = roadline_pts(img, mbar[0], bbar[0])
+    r_pts = roadline_pts(img, mbar[1], bbar[1])
+    cv2.line(image, l_pts[0], l_pts[1], (0, 255, 0), 5)
+    cv2.line(image, r_pts[0], r_pts[1], (0, 255, 0), 5)
+    return image
+
+def process_image(img0):
+    img1 = grayscale_image(img0, **theta)
+    img2 = blur_image(img1, **theta)
+    img3 = edge_image(img2, **theta)
+    img4 = mask_image(img3, road(image)[:,:,::-1], **theta)
+    img5, lines = houghtransform_image(img4, **theta)
+    img6 = util.weighted_img(img5, img0)
+    img7 = average_lines(img6, lines, **theta)
     return img7
+
+theta = {'kernel_size':5,
+         'low_threshold':50,
+         'high_threshold':150,
+         'rho':2,
+         'theta':1,
+         'threshold':50,
+         'min_line_length':3,
+         'max_line_gap':1}
 
 for path in glob.glob('test_images/solid*.jpg'):
     fname = path.split("/")[1]
