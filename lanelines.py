a3b89010d2ef8345b1450f015b889030fff6bcf4
David A. Ventimiglia <dventimi@gmail.com>
2016-12-01 17:30:08 -0800

    Reduced the line jitter in the movie

--- lanelines.py
+++ lanelines.py
@@ -1,11 +1,14 @@
-from moviepy.editor import VideoFileClip
 from IPython.display import HTML
+from moviepy.editor import VideoFileClip
+import cv2
+import math
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pdb
 import util
-import cv2
+import glob
 
 top = lambda img: 0
 bottom = lambda img: img.shape[0]
@@ -31,16 +34,26 @@ road = lambda img:  np.array([[[horizon(img), centerline(img)-0.10*width(img)/2]
 
 y = lambda x,m,b: m*x+b
 x = lambda y,m,b: (y-b)/m
+
+# def y(x,m,b):
+#     if math.isnan(x) or math.isnan(m) or math.isnan(b):
+#         pdb.set_trace()
+#     return m*x+b
+
+# def x(y,m,b):
+#     if math.isnan(y) or math.isnan(m) or math.isnan(b):
+#         pdb.set_trace()
+#     return (y-b)/m
+
 roadline_pts = lambda img,m,b: ((int(x(bottom(img),m,b)), bottom(img)),
                                 (int(x(horizon(img),m,b)), horizon(img)))
 slope = lambda lines:  (lines[:,0,3]-lines[:,0,1])/(lines[:,0,2]-lines[:,0,0])
-weight = lambda lines:  np.sqrt((lines[:,0,2]-lines[:,0,0])**2+(lines[:,0,3]-lines[:,0,1])**2)
 intercept = lambda lines, m:  lines[:,0,1]-m*lines[:,0,0]
 
 lidx = lambda slopes:  np.logical_and(np.isfinite(slopes), slopes<0)
 ridx = lambda slopes:  np.logical_and(np.isfinite(slopes), slopes>0)
 
-def process_image(img0, kernel_size=5, low_threshold=50, high_threshold=150, rho=2, theta=1, threshold=100, min_line_length=3, max_line_gap=1):
+def process_image(img0, kernel_size=5, low_threshold=50, high_threshold=150, rho=2, theta=1, threshold=50, min_line_length=3, max_line_gap=1):
     img1 = util.grayscale(img0)
     img2 = util.gaussian_blur(img1, kernel_size)
     img3 = util.canny(img2, low_threshold, high_threshold)
@@ -50,21 +63,23 @@ def process_image(img0, kernel_size=5, low_threshold=50, high_threshold=150, rho
     img7 = np.copy(img6)
     m = slope(lines)
     b = intercept(lines, m)
-    w = weight(lines)
     mbar = [np.mean(m[lidx(m)]), np.mean(m[ridx(m)])]
     bbar = [np.mean(b[lidx(m)]), np.mean(b[ridx(m)])]
+    if math.isnan(mbar[0]):
+        pdb.set_trace()
     l_pts = roadline_pts(img0, mbar[0], bbar[0])
     r_pts = roadline_pts(img0, mbar[1], bbar[1])
     cv2.line(img7, l_pts[0], l_pts[1], (0, 255, 0), 5)
     cv2.line(img7, r_pts[0], r_pts[1], (0, 255, 0), 5)
     return img7
 
-for f in os.listdir('test_images'):
-    image = mpimg.imread('test_images/solidWhiteRight.jpg')
+for path in glob.glob('test_images/solid*.jpg'):
+    fname = path.split("/")[1]
+    image = mpimg.imread(path)
     processed_image = process_image(image)
-    mpimg.imsave("test_images/processed_%s" % f, processed_image)
+    mpimg.imsave("test_images/processed_%s" % fname, processed_image)
 
-# white_output = 'white.mp4'
-# clip1 = VideoFileClip("solidWhiteRight.mp4")
-# white_clip = clip1.fl_image(process_image)
-# white_clip.write_videofile(white_output, audio=False)
+white_output = 'white.mp4'
+clip1 = VideoFileClip("solidWhiteRight.mp4")
+white_clip = clip1.fl_image(process_image)
+white_clip.write_videofile(white_output, audio=False)
